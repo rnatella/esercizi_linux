9cdf5698c7220714559156f0d4e1d46ea931051f
Roberto Natella <2117196+rnatella@users.noreply.github.com>
2021-11-12 19:28:22 +0100

    Refactoring

--- ambiente_globale/produttore_consumatore/coppia_di_buffer/buffer.c
+++ ambiente_globale/produttore_consumatore/coppia_di_buffer/buffer.c
@@ -6,51 +6,46 @@
 void produzione(int sem_id, buffer *buf1, buffer *buf2, int valore)
 {
 
+    buffer * buf;
+
     Wait_Sem(sem_id, SPAZIO_DISP);
 
-    /* Non è necessario qui acquisire un MUTEX_P
-     * per la ricerca di un buffer LIBERO, vi è un
-     * solo produttore.
+
+    /* RICERCA DEL BUFFER LIBERO */
+
+    /* NOTA: Nel caso ci fossero stati più di un produttore,
+     * sarebbe necessario qui usare la mutua esclusione
+     * (MUTEX_P) per cerca il buffer LIBERO
      */
 
     if (buf1->stato == LIBERO)
     {
-        buf1->stato = INUSO;
-
         printf("[%d] In produzione su buf1...\n", getpid());
 
-        sleep(1);
+        buf = buf1;
 
-        buf1->valore = valore;
-
-        buf1->stato = OCCUPATO;
-
-        printf("[%d] Prodotto il valore %d su buf1\n", getpid(), valore);
-    }
-    else
-    {
-
-        /* se il programma è arrivato a questo punto, vi
-         * deve essere almeno uno spazio disponibile, e 
-         * quello spazio non è in "buf1", per cui "buf2"
-         * è necessariamente libero.
-         */
-
-        buf2->valore = valore;
+    } else { /* buf2->stato == LIBERO */
 
         printf("[%d] In produzione su buf2...\n", getpid());
 
-        buf2->stato = INUSO;
-
-        sleep(1);
-
-        buf2->valore = valore;
-
-        buf2->stato = OCCUPATO;
-
-        printf("[%d] Prodotto il valore %d su buf2\n", getpid(), valore);
-
+        buf = buf2;
     }
+
+    buf->stato = INUSO;
+
+    /* FINE RICERCA DEL BUFFER LIBERO */
+
+
+
+    sleep(1);
+
+    buf->valore = valore;
+
+    buf->stato = OCCUPATO;
+
+    printf("[%d] Prodotto il valore %d\n", getpid(), valore);
+
+
 
     Signal_Sem(sem_id, MESSAGGIO_DISP);
 }
@@ -58,53 +53,51 @@ void produzione(int sem_id, buffer *buf1, buffer *buf2, int valore)
 int consumazione(int sem_id, buffer *buf1, buffer *buf2)
 {
 
+    buffer * buf;
+
     int valore;
 
     Wait_Sem(sem_id, MESSAGGIO_DISP);
 
-    /* Non è necessario qui acquisire un MUTEX_C
-     * per la ricerca di un buffer OCCUPATO, vi è un
-     * solo consumatore.
+
+    /* RICERCA DEL BUFFER OCCUPATO */
+
+    /* NOTA: Nel caso ci fossero stati più di un consumatore,
+     * sarebbe necessario qui usare la mutua esclusione
+     * (MUTEX_C) per cerca il buffer OCCUPATO
      */
 
     if (buf1->stato == OCCUPATO)
     {
-        buf1->stato = INUSO;
-
         printf("[%d] In consumazione su buf1...\n", getpid());
 
-        sleep(2);
+        buf = buf1;
 
-        valore = buf1->valore;
-
-        buf1->stato = LIBERO;
-
-        printf("[%d] Consumato il valore %d su buf1\n", getpid(), valore);
-
-    }
-    else
-    {
-        /* se il programma è arrivato a questo punto, vi
-         * deve essere almeno un messaggio disponibile, e 
-         * quel messaggio non è in "buf1", per cui "buf2"
-         * è necessariamente occupato.
-         */
-
-        buf1->stato = INUSO;
+    } else { /* buf2->stato == OCCUPATO */
 
         printf("[%d] In consumazione su buf2...\n", getpid());
 
-        sleep(2);
-
-        valore = buf2->valore;
-
-        buf2->stato = LIBERO;
-
-        printf("[%d] Consumato il valore %d su buf2\n", getpid(), valore);
-
+        buf = buf2;
     }
+
+    buf->stato = INUSO;
+
+    /* FINE RICERCA DEL BUFFER OCCUPATO */
+
+
+
+    sleep(2);
+
+    valore = buf->valore;
+
+    buf->stato = LIBERO;
+
+    printf("[%d] Consumato il valore %d\n", getpid(), valore);
+
+
 
     Signal_Sem(sem_id, SPAZIO_DISP);
 
     return valore;
 }
+
