ca10668082a2b7ae4dcefc26e73abde8fb649f80
Roberto Natella <roberto.natella@unina.it>
2023-09-03 19:59:37 +0200

    prod cons priorità

--- ambiente_globale/produttore_consumatore/produttore-consumatore_con_priorita/procedure.c
+++ ambiente_globale/produttore_consumatore/produttore-consumatore_con_priorita/procedure.c
@@ -34,62 +34,65 @@ void produci_alta_prio(PriorityProdCons* p){
 
 	enter_monitor(&p->m);
 
-	//Aumenta il numero di produttori
-	printf("Produttore 1 entrato nel monitor\n");
+	printf("Produttore tipo 1 con pid %d accede al monitor\n", getpid());
 
-	//Attendo che il vettore non sia pieno
+	// Attendo che il vettore non sia pieno
 	while(p->count == DIM) {
 		p->num_produttori_alta_prio++;
 		wait_condition(&p->m,NOT_FULL_1);
 		p->num_produttori_alta_prio--;
 	}
 
-	//Produco il valore
-	value = rand() % 12 ;
+	// Produzione
 
-	//Modifico il buffer
+	value = rand() % 12;
+
 	p->buffer[p->testa] = value;
 	p->testa = (p->testa + 1) % DIM;
 	p->count++;
 
-	//Il buffer non è vuoto
-	printf("Produttore 1 con pid %d ha prodotto %d\n",getpid(),value);
+	printf("Produttore tipo 1 con pid %d ha prodotto %d\n",getpid(),value);
 
-	//Segnalo quelli che mi aspettano che ho finito
+
+	// Riattivazione di un consumatore
 	signal_condition(&p->m,NOT_EMPTY);
 
-	//Segnalo che c'è almeno un elemento disponibile
+
 	leave_monitor(&p->m);
+
 }
 
 void produci_bassa_prio(PriorityProdCons* p){
 
 	int value;
+
 	enter_monitor(&p->m);
 
-	//Aumenta il numero di produttori
-	printf("Produttore 2 entrato nel monitor\n");
+	printf("Produttore tipo 2 con pid %d accede al monitor\n", getpid());
+
 
 	//Attendo che il vettore NON sia pieno, è che NON vi siano altri produttori ad alta priorità in attesa
+
 	while(p->count == DIM || p->num_produttori_alta_prio > 0) {
 		wait_condition(&p->m,NOT_FULL_2);
 	}
 
-	//Produco il valore
+
+	// Produzione
+
 	value = 13 + (rand() % 12) ;
 
-	//Modifico il buffer
 	p->buffer[p->testa] = value;
 	p->testa = (p->testa + 1) % DIM;
 	p->count++;
 
-	//Il buffer non è vuoto
-	printf("Produttore 2 con pid %d ha prodotto %d\n",getpid(),value);
+	printf("Produttore tipo 2 con pid %d ha prodotto %d\n", getpid(), value);
 
-	//Segnalo quelli che mi aspettano che ho finito
+
+	// Riattivazione di un consumatore
 	signal_condition(&p->m,NOT_EMPTY);
 
-	//Segnalo che c'è almeno un elemento disponibile
+
 	leave_monitor(&p->m);
 
 }
@@ -100,26 +103,35 @@ void consuma(PriorityProdCons* p){
 
 	enter_monitor(&p->m);
 
+	printf("Consumatore con pid %d accede al monitor\n", getpid());
+
+
 	//Se non c'è nulla nel vettore, attendo che qualcuno produca
 	while(p->count == 0){
 		wait_condition(&p->m,NOT_EMPTY);
 	}
 
+
+	// Consumazione
+
 	value = p->buffer[p->coda];
 	p->coda = (p->coda + 1) % DIM;
 	p->count--;
 
-	printf("Consumatore con pid %d ha consumato valore %d\n",getpid(),value);
+	printf("Consumatore con pid %d ha consumato %d\n", getpid(), value);
+
 
 	printf("Processi in coda nella variabile NOT_FULL_1 %d\n",queue_condition(&p->m,NOT_FULL_1));
 	printf("Processi in coda nella variabile NOT_FULL_2 %d\n",queue_condition(&p->m,NOT_FULL_2));
 
-	//Ho consumato quindi se ci sono produttori di tipo 1 producono loro
-	//altrimenti producono quelli di tipo 2
+	// Si riattiva un produttore di tipo 1 se presente,
+	// altrimenti si riattiva un produttore di tipo 2
 	if(p->num_produttori_alta_prio > 0)
 		signal_condition(&p->m,NOT_FULL_1);
 	else
 		signal_condition(&p->m,NOT_FULL_2);
 
+
 	leave_monitor(&p->m);
+
 }
--- ambiente_globale/produttore_consumatore/produttore-consumatore_con_priorita/pthreads/prodcons.c
+++ ambiente_globale/produttore_consumatore/produttore-consumatore_con_priorita/pthreads/prodcons.c
@@ -12,10 +12,10 @@ void inizializza_prod_cons(PriorityProdCons* p){
 	p->testa = 0;
 	p->coda = 0;
     p->count = 0;
-    
+
     p->threads_prio_1 = 0;
     p->threads_prio_2 = 0;
-    
+
     pthread_mutex_init( &(p->mutex), NULL );
 	pthread_cond_init( &(p->not_full_prio1), NULL );
 	pthread_cond_init( &(p->not_full_prio2), NULL );
@@ -29,7 +29,7 @@ void rimuovi_prod_cons(PriorityProdCons* p){
 	pthread_cond_destroy( &(p->not_full_prio1) );
 	pthread_cond_destroy( &(p->not_full_prio2) );
     pthread_cond_destroy( &(p->not_empty) );
-    
+
 }
 
 void produci_alta_prio(PriorityProdCons* p){
@@ -37,48 +37,49 @@ void produci_alta_prio(PriorityProdCons* p){
 	int value;
 
     /* Ingresso nel monitor */
-    
+
 	pthread_mutex_lock(&(p->mutex));
-    
+
+	printf("Produttore tipo 1 accede al monitor\n");
 
 
     /* Controlla la condizione logica: il buffer NON deve essere pieno */
 
 	while( p->count == DIM ) {
-        
+
         p->threads_prio_1++;
-        
-        printf("Produzione priorità alta in attesa\n");
-        
+
+        printf("Produttore tipo 1 in attesa\n");
+
 		pthread_cond_wait(&(p->not_full_prio1), &(p->mutex));
-        
-        printf("Produzione priorità alta risvegliato\n");
-	    
+
+        printf("Produttore tipo 1 risvegliato\n");
+
         p->threads_prio_1--;
     }
 
-    
+
 
     /* Produzione */
-    
+
 	value = rand() % 12;
 
 	p->buffer[p->testa] = value;
 	p->testa = (p->testa + 1) % DIM;
     p->count++;
 
-    printf("Produzione priorità alta: %d\n", value);
+	printf("Produttore tipo 1 ha prodotto %d\n", value);
 
-    
-    
+
+
     /* Sveglia un eventuale consumatore in attesa */
-    
+
 	pthread_cond_signal(&(p->not_empty));
 
 
-    
+
     /* Uscita dal monitor */
-    
+
     pthread_mutex_unlock(&(p->mutex));
 }
 
@@ -86,128 +87,126 @@ void produci_alta_prio(PriorityProdCons* p){
 
 
 void produci_bassa_prio(PriorityProdCons* p){
-	
-    int value;
-    
-    /* Ingresso nel monitor */
-    
-	pthread_mutex_lock(&(p->mutex));
-    
 
-    
+    int value;
+
+    /* Ingresso nel monitor */
+
+	pthread_mutex_lock(&(p->mutex));
+
+	printf("Produttore tipo 2 accede al monitor\n");
+
+
     /* Controlla la condizione logica: il buffer NON deve essere pieno, e NON ci devono essere produttori ad alta priorità in attesa
      */
-    
+
     /* NOTA BENE: a differenza dei produttori ad alta priorità, i produttori a bassa priorità devono assicurarsi che non siano già presenti dei produttori ad alta priorità ad attendere, usando la condizione "p->threads_prio_1 > 0".
-     
+
        Questo controllo è importante nel seguente scenario:
-     
+
         - Si supponga che un thread consumatore abbia appena risvegliato un thread produttore ad alta priorità.
 
         - Poiché il monitor è di tipo signal-and-continue, il thread consumatore non rilascia subito il monitor al momento della signal_condition, ed il thread produttore appena risvegliato si mette in attesa sul mutex del monitor.
 
         - Se nel frattempo sopraggiunge un thread produttore a bassa priorità, si mette anche esso in attesa sul mutex del monitor.
-     
+
         - Quando il thread consumatore esce dal monitor, il thread produttore a bassa priorità potrebbe entrare nel monitor PRIMA del thread produttore ad alta priorità. Ciò può accadere poiché l'operazione di mutex_lock non fornisce alcuna garanzia sull'ordine con cui i thread acquisiranno il mutex. In altre parole, se due thread effettuano mutex_lock su un mutex bloccato, quando il lock viene sbloccato non è possibile prevedere quale dei due thread acquisirà il mutex.
-     
+
         - Aggiungendo il controllo sul numero di produttori ad alta priorità in attesa, i thread produttori a bassa priorità lasciano il posto ad un eventuale thread produttore ad alta priorità in attesa.
      */
-    
+
 	while( p->count == DIM || p->threads_prio_1 > 0 ) {
-        
+
         p->threads_prio_2++;
-        
-        printf("Produzione priorità bassa in attesa\n");
-        
+
+        printf("Produttore tipo 2 in attesa\n");
+
 		pthread_cond_wait(&(p->not_full_prio2), &(p->mutex));
-        
-        printf("Produzione priorità bassa risvegliato\n");
-        
+
+        printf("Produttore tipo 2 risvegliato\n");
+
         p->threads_prio_2--;
 	}
 
-    
-    
+
+
     /* Produzione */
-    
+
 	value = 13 + (rand() % 12);
 
 	p->buffer[p->testa] = value;
 	p->testa = (p->testa + 1) % DIM;
     p->count++;
-	
-    printf("Produzione priorità bassa: %d\n", value);
-    
-    
-    
-    
+
+	printf("Produttore tipo 2 ha prodotto %d\n", value);
+
+
+
     /* Sveglia un eventuale consumatore in attesa */
-    
+
 	pthread_cond_signal(&(p->not_empty));
 
 
-    
+
     /* Uscita dal monitor */
-	
+
     pthread_mutex_unlock(&(p->mutex));
 
 }
 
-
-
-
 void consuma(PriorityProdCons* p){
-    
-	int value;
-    
-    /* Ingresso nel monitor */
-    
-	pthread_mutex_lock(&(p->mutex));
-    
 
-    
+	int value;
+
+    /* Ingresso nel monitor */
+
+	pthread_mutex_lock(&(p->mutex));
+
+	printf("Consumatore accede al monitor\n");
+
+
     /* Controlla la condizione logica: il buffer NON deve essere vuoto */
-    
+
 	while( p->count == 0 ) {
-        
+
         printf("Consumatore in attesa\n");
-        
+
 		pthread_cond_wait(&(p->not_empty), &(p->mutex));
 
         printf("Consumatore risvegliato\n");
     }
 
-    
-    
+
+
     /* Consumazione */
-    
+
 	value = p->buffer[p->coda];
 	p->coda = (p->coda + 1) % DIM;
-    p->count--;
+	p->count--;
 
-	printf("Consumato valore %d\n", value);
+	printf("Consumatore ha consumato %d\n", value);
 
-    
-    
+
+
     /* Se presenti, sveglia un thread produttore ad alta priorità,
        altrimenti sveglia un thread produttore a bassa priorità */
-    
+
 	if(p->threads_prio_1 > 0) {
-        
+
         printf("Consumatore risveglia produttore ad alta priorità\n");
-        
+
 		pthread_cond_signal(&(p->not_full_prio1));
-        
+
 	} else if(p->threads_prio_2 > 0) {
-        
+
         printf("Consumatore risveglia produttore a bassa priorità\n");
-        
+
         pthread_cond_signal(&(p->not_full_prio2));
     }
 
-    
+
     /* Uscita dal monitor */
-    
+
 	pthread_mutex_unlock(&(p->mutex));
-    
+
 }
