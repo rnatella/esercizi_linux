905b0dfeae505f8cff7e0ecf06c73a775569748b
Roberto Natella <roberto.natella@unina.it>
2023-11-28 09:18:24 +0100

    serversync

--- ambiente_locale/code_messaggi/server_sincroni_multipli/serversync.c
+++ ambiente_locale/code_messaggi/server_sincroni_multipli/serversync.c
@@ -11,14 +11,15 @@ void send_sinc(int ok_id, int req_id, messaggio * msg)
 
     int ret;
 
+    pid_t pid = getpid();
+
     request_to_send req_msg;
     ok_to_send ok_msg;
 
 
+    printf("[%d] Client: invio request-to-send\n", pid);
 
-    req_msg.type = getpid();
-
-    printf("[%d] Client: invio request-to-send, type=%ld\n", getpid(), req_msg.type);
+    req_msg.type = pid;
 
     ret = msgsnd(req_id, &req_msg, sizeof(request_to_send) - sizeof(long), 0);
 
@@ -29,11 +30,10 @@ void send_sinc(int ok_id, int req_id, messaggio * msg)
     }
 
 
-    printf("[%d] Client: in attesa di ok-to-send...\n", getpid());
+
+    printf("[%d] Client: in attesa di ok-to-send...\n", pid);
 
     ret = msgrcv(ok_id, &ok_msg, sizeof(ok_to_send) - sizeof(long), getpid(), 0);
-
-    printf("[%d] Client: ricevuto ok-to-send... type=%ld, id_coda=%d\n", getpid(), ok_msg.type, ok_msg.id_coda);
 
     if (ret < 0)
     {
@@ -41,11 +41,14 @@ void send_sinc(int ok_id, int req_id, messaggio * msg)
         exit(1);
     }
 
-
-
+    long type = ok_msg.type;
     int id_coda = ok_msg.id_coda;
 
-    printf("[%d] Client: invio messaggio, coda=%d, type=%ld, valore=%d\n", getpid(), id_coda, msg->type, msg->val);
+    printf("[%d] Client: ricevuto ok-to-send... type=%ld, id_coda=%d\n", pid, type, id_coda);
+
+
+
+    printf("[%d] Client: invio messaggio, coda=%d, type=%ld, valore=%d\n", pid, id_coda, msg->type, msg->val);
 
     ret = msgsnd(id_coda, msg, sizeof(messaggio) - sizeof(long), 0);
 
@@ -62,6 +65,8 @@ void receive_sinc(int msg_id, int ok_id, int req_id, messaggio * msg)
 
     int ret;
 
+    pid_t pid = getpid();
+
     request_to_send req_msg;
     ok_to_send ok_msg;
 
@@ -70,20 +75,26 @@ void receive_sinc(int msg_id, int ok_id, int req_id, messaggio * msg)
 
     ret = msgrcv(req_id, &req_msg, sizeof(request_to_send) - sizeof(long), 0, 0);
 
-    printf("[%d] Server: ricevuto request-to-send, type=%ld\n", getpid(), req_msg.type);
-
     if (ret < 0)
     {
         perror("Errore msgrcv (request-to-send)");
         exit(1);
     }
 
+    pid_t pid_client = req_msg.type;
 
+    printf("[%d] Server: ricevuto request-to-send, type=%d\n", pid, pid_client);
+
+
+
+
+    long type = req_msg.type;
+    int id_coda = msg_id;
+
+    printf("[%d] Server: invio ok-to-send, type=%ld, id_coda=%d\n", pid, type, id_coda);
 
     ok_msg.type = req_msg.type;
     ok_msg.id_coda = msg_id;
-
-    printf("[%d] Server: invio ok-to-send, type=%ld, id_coda=%d\n", getpid(), ok_msg.type, ok_msg.id_coda);
 
     ret = msgsnd(ok_id, &ok_msg, sizeof(ok_to_send) - sizeof(long), 0);
 
@@ -94,16 +105,17 @@ void receive_sinc(int msg_id, int ok_id, int req_id, messaggio * msg)
     }
 
 
-    printf("[%d] Server: in attesa del messaggio...\n", getpid());
+
+    printf("[%d] Server: in attesa del messaggio...\n", pid);
 
     ret = msgrcv(msg_id, msg, sizeof(messaggio) - sizeof(long), 0, 0);
-
-    printf("[%d] Server: ricevuto messaggio, type=%ld, valore=%d\n", getpid(), msg->type, msg->val);
 
     if (ret < 0)
     {
         perror("Errore msgrcv (coda messaggi)");
         exit(1);
     }
+
+    printf("[%d] Server: ricevuto messaggio, type=%ld, valore=%d\n", pid, msg->type, msg->val);
 
 }
